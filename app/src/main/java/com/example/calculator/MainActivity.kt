cff27fbd19889781c972dc8e7b91210947a7aa11
M-Ameen <ameen.akbar2000@gmail.com>
2022-07-03 10:24:27 +0500

    Added functionality for onDecimalPoint()

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,19 +3,50 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import com.example.calculator.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    var lastnumeric = false
+    var lastdot = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+
     }
 
     fun onDigit(view: View) {
-        Toast.makeText(getApplicationContext(), "onDigit Is Executed", Toast.LENGTH_SHORT).show()
+        binding.tvinput.append((view as Button).text)
+        lastnumeric = true
     }
-    fun onEqual(view: View) {}
+    fun onEqual(view: View) {
+    }
+
+    fun onDecimalPoint(view: View) {
+        if(lastnumeric && !lastdot){
+            binding.tvinput.append(".")
+            lastnumeric = false
+            lastdot = true
+        }
+    }
+
     fun onOperator(view: View) {}
-    fun onDecimalPoint(view: View) {}
-    fun onClear(view: View) {}
+
+    fun onClear(view: View) {
+        binding.tvinput.text=""
+        lastdot = false
+        lastnumeric = false
+    }
 }
