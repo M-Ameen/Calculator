fa638680faf752a8a01a1c3f1bd397bc7d978a4a
M-Ameen <ameen.akbar2000@gmail.com>
2022-07-04 01:11:42 +0500

    Added functionality for all functions Finalizes the calculator app

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     //These Booleans are flags to control the onDecimal and onOperator functions
-    var lastnumeric = false
+    var lastNumeric = false
     var lastdot = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,23 +30,108 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit(view: View) {
         binding.tvinput.append((view as Button).text)
-        lastnumeric = true
+        lastNumeric = true
     }
-    fun onEqual(view: View) {
+    fun onEqual(view: View) {// If the last input is a number only, solution can be found.
+    if (lastNumeric) {
+        // Read the textView value
+        var value = binding.tvinput.text.toString()
+        var prefix = ""
+        try {
+
+            // Here if the value starts with '-' then we will separate it and perform the calculation with value.
+            if (value.startsWith("-")) {
+                prefix = "-"
+                value = value.substring(1);
+            }
+
+            // If the inputValue contains the Division operator
+            if (value.contains("/")) {
+                // Will split the inputValue using Division operator
+                val splitedValue = value.split("/")
+
+                var one = splitedValue[0] // Value One
+                val two = splitedValue[1] // Value Two
+
+                if (!prefix.isEmpty()) { // If the prefix is not empty then we will append it with first value i.e one.
+                    one = prefix + one
+                }
+
+                /*Here as the value one and two will be calculated based on the operator and
+                if the result contains the zero after decimal point will remove it.
+                And display the result to TextView*/
+                binding.tvinput.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+            } else if (value.contains("*")) {
+                // If the inputValue contains the Multiplication operator
+                // Will split the inputValue using Multiplication operator
+                val splitedValue = value.split("*")
+
+                var one = splitedValue[0] // Value One
+                val two = splitedValue[1] // Value Two
+
+                if (!prefix.isEmpty()) { // If the prefix is not empty then we will append it with first value i.e one.
+                    one = prefix + one
+                }
+
+                /*Here as the value one and two will be calculated based on the operator and
+                if the result contains the zero after decimal point will remove it.
+                And display the result to TextView*/
+                binding.tvinput.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+            } else if (value.contains("-")) {
+
+                // If the inputValue contains the Subtraction operator
+                // Will split the inputValue using Subtraction operator
+                val splitedValue = value.split("-")
+
+                var one = splitedValue[0] // Value One
+                val two = splitedValue[1] // Value Two
+
+                if (!prefix.isEmpty()) { // If the prefix is not empty then we will append it with first value i.e one.
+                    one = prefix + one
+                }
+
+                /*Here as the value one and two will be calculated based on the operator and
+                if the result contains the zero after decimal point will remove it.
+                And display the result to TextView*/
+                binding.tvinput.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+            } else if (value.contains("+")) {
+                // If the inputValue contains the Addition operator
+                // Will split the inputValue using Addition operator
+                val splitedValue = value.split("+")
+
+                var one = splitedValue[0] // Value One
+                val two = splitedValue[1] // Value Two
+
+                if (!prefix.isEmpty()) { // If the prefix is not empty then we will append it with first value i.e one.
+                    one = prefix + one
+                }
+
+                /*Here as the value one and two will be calculated based on the operator and
+                if the result contains the zero after decimal point will remove it.
+                And display the result to TextView*/
+                binding.tvinput.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+            }
+        } catch (e: ArithmeticException) {
+            e.printStackTrace()
+        }
     }
+}
+
+
+
 
     fun onDecimalPoint(view: View) {
-        if(lastnumeric && !lastdot){
+        if(lastNumeric && !lastdot){
             binding.tvinput.append(".")
-            lastnumeric = false
+            lastNumeric = false
             lastdot = true
         }
     }
 
     fun onOperator(view: View) {
-        if (lastnumeric && !isOperatorAdded(binding.tvinput.text.toString())){
+        if (lastNumeric && !isOperatorAdded(binding.tvinput.text.toString())){
             binding.tvinput.append((view as Button).text)
-            lastnumeric = false
+            lastNumeric = false
             lastdot = false
         }
     }
@@ -54,7 +139,7 @@ class MainActivity : AppCompatActivity() {
     fun onClear(view: View) {
         binding.tvinput.text=""
         lastdot = false
-        lastnumeric = false
+        lastNumeric = false
     }
 
     private fun isOperatorAdded(value: String): Boolean{
@@ -63,5 +148,19 @@ class MainActivity : AppCompatActivity() {
         }else{
             value.contains("/") || value.contains("*") || value.contains("+") || value.contains("-")
         }
+    }
+
+    /**
+     * Remove the zero after decimal point
+     */
+    private fun removeZeroAfterDot(result: String): String {
+
+        var value = result
+
+        if (result.contains(".0")) {
+            value = result.substring(0, result.length - 2)
+        }
+
+        return value
     }
 }
